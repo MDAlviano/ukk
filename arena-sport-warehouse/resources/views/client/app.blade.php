421d6584245a71fdebef66eb7434620916f6c607
MDAlviano <mdalviano.21122@gmail.com>
2025-09-27 13:29:23 +0700

    add: footer

--- arena-sport-warehouse/resources/views/client/app.blade.php
+++ arena-sport-warehouse/resources/views/client/app.blade.php
@@ -77,21 +77,21 @@
     <div id="services" class="flex flex-col gap-6">
         <h1 class="text-xl font-semibold">Layanan yang dapat Membantumu</h1>
         <div class="flex flex-row justify-between">
-            <div class="flex flex-col gap-2 justify-center rounded-xl bg-white shadow-xl">
+            <div class="flex flex-col gap-2 justify-center rounded-xl bg-white shadow-xl hover:scale-105 transition duration-300 cursor-pointer">
                 <div class="flex flex-col gap-2 p-5">
                     <h5 class="text-2xl font-medium">Frequently Asked Question</h5>
                     <p class="text-sm">Memastikan pertanyaan mu terjawab</p>
                 </div>
                 <img src="{{ asset('/assets/faq.png') }}" alt="category image">
             </div>
-            <div class="flex flex-col gap-2 justify-center rounded-xl bg-white shadow-xl">
+            <div class="flex flex-col gap-2 justify-center rounded-xl bg-white shadow-xl hover:scale-105 transition duration-300 cursor-pointer">
                 <div class="flex flex-col gap-2 p-5">
                     <h5 class="text-2xl font-medium">Online Payment Process</h5>
                     <p class="text-sm">Memudahkan mu dalam melakukan pembayaran</p>
                 </div>
                 <img src="{{ asset('/assets/online-payment.png') }}" alt="category image">
             </div>
-            <div class="flex flex-col gap-2 justify-center rounded-xl bg-white shadow-xl">
+            <div class="flex flex-col gap-2 justify-center rounded-xl bg-white shadow-xl hover:scale-105 transition duration-300 cursor-pointer">
                 <div class="flex flex-col gap-2 p-5">
                     <h5 class="text-2xl font-medium">Home Delivery Options</h5>
                     <p class="text-sm">Pastikan pesananmu sampai di depan rumah</p>
@@ -103,8 +103,45 @@
 </main>
 
 {{-- footer --}}
-<footer>
-
+<footer class="mt-20 bg-gray-300 py-10 px-16 flex flex-row gap-20">
+    <div class="font-semibold">
+        <h1 class="text-vibrant-orange text-xl">Arena Sport Warehouse</h1>
+        <h4 class="opacity-80">Tempat belanja favoritmu!</h4>
+    </div>
+    <div class="flex flex-col gap-2">
+        <h1 class="text-xl font-semibold">Links</h1>
+        <span class="w-full h-[0.7px] bg-dark-gray"></span>
+        <div class="flex flex-col gap-1 text-gray-500">
+            <a href="/" class="hover:underline">
+                Home
+            </a>
+            <a href="/categories" class="hover:underline">
+                Categories
+            </a>
+            <a href="/products" class="hover:underline">
+                Products
+            </a>
+            <a href="/about" class="hover:underline">
+                About
+            </a>
+            <a href="/profile" class="hover:underline">
+                Profile
+            </a>
+        </div>
+    </div>
+    <div class="flex flex-col gap-2">
+        <h1 class="text-xl font-semibold">Follow Us</h1>
+        <span class="w-full h-[0.7px] bg-dark-gray"></span>
+        <div class="flex flex-row gap-1 items-center text-gray-500">
+            <img src="{{ asset('/assets/instagram.svg') }}" alt="" class="h-5">
+            <a href="/" class="hover:underline">@sportwarehouse</a>
+        </div>
+    </div>
+    <div class="flex flex-col gap-2">
+        <h1 class="text-xl font-semibold">Location</h1>
+        <span class="w-full h-[0.7px] bg-dark-gray"></span>
+        <p class="text-gray-500">Jl. Pemuda no 12 Kota Semarang Jawa Tengah</p>
+    </div>
 </footer>
 </body>
 </html>
