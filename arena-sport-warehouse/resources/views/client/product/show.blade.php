e995f06482f919cdedebc2fa3b4d5eb068d85253
MDAlviano <mdalviano.21122@gmail.com>
2025-11-13 12:53:15 +0700

    fix

--- arena-sport-warehouse/resources/views/client/product/show.blade.php
+++ arena-sport-warehouse/resources/views/client/product/show.blade.php
@@ -35,62 +35,65 @@
 {{-- main --}}
 <main class="py-10 px-16 flex flex-col gap-16">
     {{-- product data --}}
-    <div class="flex flex-row gap-10 items-center">
+    <div class="flex flex-row gap-12 items-center">
         {{-- image --}}
-        <img src="{{ asset('/assets/placeholder.png') }}" alt="img" class="h-96 rounded-lg">
+        <div class="w-2/5 rounded-lg object-cover aspect-square overflow-hidden">
+            <img src="{{ asset('/assets/placeholder.png') }}" alt="img" class="size-full hover:scale-105 hover:opacity-85 transition duration-200 object-cover">
+        </div>
         {{-- data --}}
-        <div class="flex flex-col gap-4">
+        <div class="flex flex-col gap-5">
             <h1 class="text-4xl font-bold">Raket Yonex</h1>
-            <p class="text-dark-gray font-medium">⭐ 5.0/5.0 (125 Ulasan)</p>
-            <h3 class="text-2xl font-medium text-vibrant-orange">Rp200.000</h3>
-            <div class="flex flex-row gap-3">
-                {{-- add to favourite btn --}}
-                <a href="">
-                    <div class="flex flex-row py-3 px-5 items-center rounded-3xl gap-2 bg-black text-white hover:bg-dark-gray transition duration-200">
-                        <img src="{{ asset('/assets/favourite-white.svg') }}" alt="shopping cart" class="size-5">
-                        <p>Add to Cart</p>
-                    </div>
-                </a>
-                {{-- add to cart btn --}}
-                <a href="">
-                    <div class="flex flex-row py-3 px-5 items-center rounded-3xl gap-2 bg-black text-white hover:bg-dark-gray transition duration-200">
-                        <img src="{{ asset('/assets/shopping-cart-white.svg') }}" alt="shopping cart" class="size-5">
-                        <p>Add to Cart</p>
-                    </div>
-                </a>
+            <div class="flex flex-row gap-5">
+                <div class="flex flex-row gap-1 items-center">
+                    <img src="{{ asset('/assets/ic_star.svg') }}" alt="star" class="size-6">
+                    <p><span class="text-[#FFB700] font-semibold text-lg">5.0 / 5.0</span></p>
+                </div>
+                <div class="flex flex-row gap-1 items-center">
+                    <img src="{{ asset('/assets/ic_bag.svg') }}" alt="star" class="size-5">
+                    <p><span class="text-dark-gray font-semibold text-lg opacity-50">{{ number_format(125) }} orders</span></p>
+                </div>
             </div>
-            <div class="flex flex-col gap-1">
-                <h1 class="font-semibold text-lg">Description:</h1>
-                <p class="text-lg">Raket Yonex terbaru yang sangat bagusRaket Yonex terbaru yang sangat bagusRaket Yonex terbaru yang
-                    sangat bagusRaket Yonex terbaru yang sangat bagusRaket Yonex terbaru yang sangat bagusRaket Yonex
-                    terbaru yang sangat bagusRaket Yonex terbaru yang sangat bagus</p>
+            <div class="flex flex-col">
+                <h1 class="font-semibold">Stok Tersedia:</h1>
+                <p class="text-3xl font-semibold">{{ number_format(10) }}</p>
+            </div>
+            <div class="flex flex-col">
+                <h1 class="font-semibold">Stok Tersedia:</h1>
+                <p class="text-3xl font-semibold">Rp{{ number_format(200000) }}</p>
+            </div>
+            <div class="flex flex-col gap-1.5">
+                <h1 class="font-semibold">Jumlah:</h1>
+                <input type="number" value="1" min="1" class="py-2 px-3 rounded-md outline-1 w-fit">
+            </div>
+            <div class="flex flex-row gap-3">
+                <button class="bg-vibrant-orange hover:opacity-90 hover:drop-shadow-sm transition duration-200 cursor-pointer flex flex-row items-center gap-3 text-white rounded-lg px-5 py-3 font-medium">
+                    <img src="{{ asset('/assets/ic_cart-white.svg') }}" alt="cart" class="size-5">
+                    <p>Tambah ke Keranjang</p>
+                </button>
+                <button class="bg-dark-gray hover:opacity-90 hover:drop-shadow-sm transition duration-200 cursor-pointer flex flex-row items-center gap-3 text-white rounded-lg px-5 py-3 font-medium">
+                    Beli Sekarang
+                </button>
+                <button class="bg-dark-gray hover:bg-pink-400 hover:drop-shadow-sm transition duration-200 cursor-pointer flex flex-row items-center gap-3 text-white rounded-lg p-3 font-medium">
+                    <img src="{{ asset('/assets/favourite-white.svg') }}" alt="cart" class="size-7">
+                </button>
             </div>
         </div>
     </div>
 
-    {{-- customer reviews --}}
-    <div class="flex flex-col gap-4">
-        <h1 class="font-semibold text-2xl">Reviews</h1>
-        {{-- reviews --}}
-        <x-review-card
-            name="Alviano"
-            rate=4.5
-            date="08 November 2025"
-            description="Product nya bagus sekali, aku suka banget. Makanya aku kasih rating 4,5 dari 5. Pas aku bawa buat main badminton bareng temen-temenku di atas rel kereta, enak banget rasanya. Kayak powerful banget gitu lho. Temen-temenku sampe pada nanya beli dimana, yaudah deh kusaranin buat beli di Toko Arena lewat web ini, gituuu. Jos deh mantep pokoknya!"
-        />
-        <x-review-card
-            name="Alviano"
-            rate=4.5
-            date="08 November 2025"
-            description="Product nya bagus sekali, aku suka banget. Makanya aku kasih rating 4,5 dari 5. Pas aku bawa buat main badminton bareng temen-temenku di atas rel kereta, enak banget rasanya. Kayak powerful banget gitu lho. Temen-temenku sampe pada nanya beli dimana, yaudah deh kusaranin buat beli di Toko Arena lewat web ini, gituuu. Jos deh mantep pokoknya!"
-        />
-        <x-review-card
-            name="Alviano"
-            rate=4.5
-            date="08 November 2025"
-            description="Product nya bagus sekali, aku suka banget. Makanya aku kasih rating 4,5 dari 5. Pas aku bawa buat main badminton bareng temen-temenku di atas rel kereta, enak banget rasanya. Kayak powerful banget gitu lho. Temen-temenku sampe pada nanya beli dimana, yaudah deh kusaranin buat beli di Toko Arena lewat web ini, gituuu. Jos deh mantep pokoknya!"
-        />
+    {{-- description --}}
+    <div class="flex flex-col gap-1.5">
+        <span class="w-full bg-dark-gray h-[1px] opacity-70 mb-6"></span>
+        <h1 class="font-semibold">Deskripsi</h1>
+        <p class="text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cilium dolore eu fugiat nulla pariatur.</p>
     </div>
+
+    <div class="flex flex-col gap-1.5 mb-10">
+        <span class="w-full bg-dark-gray h-[1px] opacity-70 mb-6"></span>
+        <h1 class="font-semibold">Deskripsi</h1>
+        <p class="text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cilium dolore eu fugiat nulla pariatur.</p>
+    </div>
+
+
 </main>
 
 {{-- footer --}}
